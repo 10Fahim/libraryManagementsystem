10f9719e730cbfbd558446514a7cff6adfd0b262
faim <afahimansari12@gmail.com>
2024-02-10 20:46:37 +0530

    first commit

---
+++ LibraryMS/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Course, Semester, Author, Book
+
+admin.site.register(Course)
+admin.site.register(Semester)
+admin.site.register(Author)
+admin.site.register(Book)
